64f751e523437682f44bc90cbac8d203b110405d
Deepak <deepaknnayak34@gmail.com>
2022-09-09 22:05:03 +0530

    Assignment15

---
+++ string.py
@@ -0,0 +1,60 @@
+# 1. Write a python script to create a String in 3 different possible ways
+# a = "Deepak Kumar"
+# b = '''Deepak Kumar'''
+# c = """Deepak Kumar"""
+# print(a,b,c, sep="\n")
+
+# 2. Write a python script to Get the characters from the start to position 5 (Given String
+# “iNeuron” using the slice syntax)
+
+# stringOne = "iNeuron"
+# print(stringOne[0:6:1])
+
+# 3. Write a python script to Get the characters from position 2 to position 5 (Given String
+# “Hello Learners” using the slice syntax)
+
+# stringOne = "Hello Learners"
+# print(stringOne[0:6:1])
+
+# 4. Write a python script to demonstrate String Concatenation adding space in between (
+# Given Strings a=”Learning” b=”Python” )
+# a = "Learning"
+# b = "Python"
+# result = a+" "+b
+# print(result)
+
+
+# 5. Write a python script to get the count of total number of characters in String a=
+# “iNeuron”
+
+# a = "INeuron"
+# count = 0
+# for i in a:
+#   count = count+1
+# print(count)
+
+# 6. Write a python script to reverse a String. (“iNeuron”)
+
+# a = "Ineuron"
+# print(a[::-1])
+
+# 7. Write a python script to determine whether a string contains a specific substring.
+# a = "Learning Python Fullstack With MysirG"
+# substring = "Fullstack"
+# print(substring in a)
+
+# 8. Write a python script to check if a string contains only numbers.
+# s = "12345"
+# print(s.isdigit())
+
+# 9. Write a python script to check if a string contains only characters of the alphabet.
+# Unable to understnd the question .. are we going to check some specfic character in the alphabet
+# or else we have to check some if it contains only alphabet
+
+# 10 Write a python script to convert an integer to a string.
+# a = 123
+# b = str(a)
+# print((b))
+
+
+
